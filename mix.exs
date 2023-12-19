ce269750f660b1b463975d278fb361302805a84b
José Valim <jose.valim@dashbit.co>
2023-12-19 14:08:18 +0100

    Release v1.0.1

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule PhoenixHTMLHelpers.Mixfile do
   use Mix.Project
 
   @source_url "https://github.com/phoenixframework/phoenix_html_helpers"
-  @version "1.0.0"
+  @version "1.0.1"
 
   def project do
     [
